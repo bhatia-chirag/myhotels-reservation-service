94eed1c866dcd42896d4025432ce65609158aa63
Chirag_Bhatia <chirag_bhatia@epam.com>
2021-06-16 15:51:16 +0530

    Initial commit.

---
+++ src/test/java/com/myhotels/reservationservice/ReservationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myhotels.reservationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
